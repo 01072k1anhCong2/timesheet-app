4368ab2878e3ee727b2398d06b8e8b362edcf5d0
Lam Cong <congvuong123456789@gmail.com>
2025-11-15 18:48:40 +0900

    an hien mat khau da xog

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,16 +1,18 @@
-// --- Firebase config ---
-const firebaseConfig = {
-  apiKey: "AIzaSyCi_64FqLHar_IBed4hSPpTJZpYvoE1NHo",
-  authDomain: "timesheet-a8dc2.firebaseapp.com",
-  databaseURL: "https://timesheet-a8dc2-default-rtdb.firebaseio.com",
-  projectId: "timesheet-a8dc2",
-  storageBucket: "timesheet-a8dc2.firebasestorage.app",
-  messagingSenderId: "637539776978",
-  appId: "1:637539776978:web:e7d71ffd9378876309b00b"
-};
-// Khởi tạo Firebase
-firebase.initializeApp(firebaseConfig);
-// Kết nối database
-const db = firebase.database();
-// Kết nối authentication (nếu dùng đăng nhập/đăng ký)
-const auth = firebase.auth();
+  // --- Toggle hiển thị mật khẩu ---
+document.addEventListener("DOMContentLoaded", () => {
+  const togglePass = document.getElementById("togglePass");
+  const passwordInput = document.getElementById("pass");
+  const eyeIcon = togglePass.querySelector("i");
+
+  togglePass.addEventListener("click", () => {
+    if(passwordInput.type === "password"){
+      passwordInput.type = "text";
+      eyeIcon.classList.remove("fa-eye");
+      eyeIcon.classList.add("fa-eye-slash");
+    } else {
+      passwordInput.type = "password";
+      eyeIcon.classList.remove("fa-eye-slash");
+      eyeIcon.classList.add("fa-eye");
+    }
+  });
+});
